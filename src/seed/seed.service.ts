5396a9f48bfe892dd1734f187f827db68b2e0d0c
NoahFaundez <noahfaundez@gmail.com>
2025-10-13 14:20:20 -0300

    feat: added pagination and seeder for data with PokeAPI

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class SeedService {
+  private readonly axios: AxiosInstance = axios;
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+  async executeSeed() {
+    await this.pokemonModel.deleteMany({});
+
+    const { data } = await this.axios.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
+    );
+
+    const pokemonsToInsert: { name: string; no: number }[] = [];
+
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no = +segments[segments.length - 2];
+      // const pokemon = await this.pokemonModel.create({ name, no });
+      pokemonsToInsert.push({ name, no });
+      console.log({ name, no });
+    });
+
+    await this.pokemonModel.insertMany(pokemonsToInsert);
+
+    return 'Seed executed';
+  }
+}
